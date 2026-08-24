a36054714bac91e817473a692d4816dde8ac3342
Vivek <krishnacreations745@gmail.com>
2026-08-24 15:34:18 +0530

    use axios to fetch the data

--- ecommerce-project/src/Pages/home/HomePage.jsx
+++ ecommerce-project/src/Pages/home/HomePage.jsx
@@ -1,15 +1,13 @@
+import axios from "axios";
 import { Header } from "../../Components/Header";
 import "./HomePage.css";
 import checkmark from "../../assets/images/icons/checkmark.png";
 import { products } from "../../../starting-code/data/products";
 export function HomePage() {
-  fetch("http://localhost:3000/api/products")
-    .then((response) => {
-      return response.json();
-    })
-    .then((data) => {
-      console.log(data);
-    });
+  axios.get("http://localhost:3000/api/products").then((response) => {
+    console.log(response.data);
+  });
+
   return (
     <>
       <link rel="icon" type="image" href="home-favicon.png" />
