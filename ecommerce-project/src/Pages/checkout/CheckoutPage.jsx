52a5f3e6759cc2319c13173e13bb03ac52bf860c
Vivek <krishnacreations745@gmail.com>
2026-08-24 20:00:57 +0530

    created payment summary component

--- ecommerce-project/src/Pages/checkout/CheckoutPage.jsx
+++ ecommerce-project/src/Pages/checkout/CheckoutPage.jsx
@@ -1,10 +1,11 @@
 import { CheckoutHeader } from "./CheckoutHeader";
 import "./CheckoutPage.css";
 import "./CheckoutHeader.css";
-import { formatMoney } from "../../utils/money";
+
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { OrderSummary } from "./OrderSummary";
+import { PaymentSummary } from "./PaymentSummary";
 
 export function CheckoutPage({ cart }) {
   const [paymentSummary, setPaymentSummary] = useState(null);
@@ -35,52 +36,7 @@ export function CheckoutPage({ cart }) {
         <div className="checkout-grid">
           <OrderSummary deliverOptions={deliverOptions} cart={cart} />
 
-          <div className="payment-summary">
-            <div className="payment-summary-title">Payment Summary</div>
-
-            {paymentSummary && (
-              <>
-                <div className="payment-summary-row">
-                  <div>Items ({paymentSummary.totalItems}):</div>
-                  <div className="payment-summary-money">
-                    {formatMoney(paymentSummary.productCostCents)}
-                  </div>
-                </div>
-
-                <div className="payment-summary-row">
-                  <div>Shipping &amp; handling:</div>
-                  <div className="payment-summary-money">
-                    {formatMoney(paymentSummary.shippingCostCents)}
-                  </div>
-                </div>
-
-                <div className="payment-summary-row subtotal-row">
-                  <div>Total before tax:</div>
-                  <div className="payment-summary-money">
-                    {formatMoney(paymentSummary.totalCostBeforeTaxCents)}
-                  </div>
-                </div>
-
-                <div className="payment-summary-row">
-                  <div>Estimated tax (10%):</div>
-                  <div className="payment-summary-money">
-                    {formatMoney(paymentSummary.taxCents)}
-                  </div>
-                </div>
-
-                <div className="payment-summary-row total-row">
-                  <div>Order total:</div>
-                  <div className="payment-summary-money">
-                    {formatMoney(paymentSummary.totalCostCents)}
-                  </div>
-                </div>
-
-                <button className="place-order-button button-primary">
-                  Place your order
-                </button>
-              </>
-            )}
-          </div>
+          <PaymentSummary paymentSummary={paymentSummary} />
         </div>
       </div>
     </>
--- ecommerce-project/src/Pages/checkout/OrderSummary.jsx
+++ ecommerce-project/src/Pages/checkout/OrderSummary.jsx
@@ -1,5 +1,6 @@
-import dayjs from "dayjs";
 import { formatMoney } from "../../utils/money";
+import dayjs from "dayjs";
+import { DeliverOptions } from "./DeliveryOptions";
 
 export function OrderSummary({ deliverOptions, cart }) {
   return (
@@ -43,39 +44,10 @@ export function OrderSummary({ deliverOptions, cart }) {
                   </div>
                 </div>
 
-                <div className="delivery-options">
-                  <div className="delivery-options-title">
-                    Choose a delivery option:
-                  </div>
-                  {deliverOptions.map((deliveryOption) => {
-                    let priceString = "FREE Shipping";
-                    if (deliveryOption.priceCents > 0) {
-                      priceString = `${formatMoney(deliveryOption.priceCents)} - Shipping`;
-                    }
-                    return (
-                      <div key={deliveryOption.id} className="delivery-option">
-                        <input
-                          type="radio"
-                          checked={
-                            deliveryOption.id === cartItem.deliveryOptionId
-                          }
-                          className="delivery-option-input"
-                          name={`delivery-option-1-${cartItem.productId}`}
-                        />
-                        <div>
-                          <div className="delivery-option-date">
-                            {dayjs(
-                              deliveryOption.estimatedDeliveryTimeMs,
-                            ).format("dddd, MMMM D")}
-                          </div>
-                          <div className="delivery-option-price">
-                            {priceString}
-                          </div>
-                        </div>
-                      </div>
-                    );
-                  })}
-                </div>
+                <DeliverOptions
+                  cartItem={cartItem}
+                  deliverOptions={deliverOptions}
+                />
               </div>
             </div>
           );
