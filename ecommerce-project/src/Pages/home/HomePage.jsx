2937a2c99d207de625b3297dca56904d683073d8
Vivek <krishnacreations745@gmail.com>
2026-08-24 18:16:37 +0530

    gwt cart from the backend and display cart quantity

--- ecommerce-project/src/Pages/home/HomePage.jsx
+++ ecommerce-project/src/Pages/home/HomePage.jsx
@@ -5,9 +5,14 @@ import "./HomePage.css";
 import checkmark from "../../assets/images/icons/checkmark.png";
 export function HomePage() {
   const [products, setProducts] = useState([]);
+  const [cart, setCart] = useState([]);
   useEffect(() => {
     axios.get("http://localhost:3000/api/products").then((response) => {
       setProducts(response.data);
+    });
+
+    axios.get("http://localhost:3000/api/cart-items").then((response) => {
+      setCart(response.data);
     });
   }, []);
 
@@ -16,7 +21,7 @@ export function HomePage() {
       <link rel="icon" type="image" href="home-favicon.png" />
       <title>Home Page</title>
 
-      <Header />
+      <Header cart={cart} />
 
       <div className="home-page">
         <div className="products-grid">
