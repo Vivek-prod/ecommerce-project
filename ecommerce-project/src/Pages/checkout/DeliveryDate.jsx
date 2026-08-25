44ef9607a0670c3802f5463675107bf284c7ae36
Vivek <krishnacreations745@gmail.com>
2026-08-25 14:35:06 +0530

    updated DeliveryDate Component

--- ecommerce-project/src/Pages/checkout/DeliveryDate.jsx
+++ ecommerce-project/src/Pages/checkout/DeliveryDate.jsx
@@ -1,11 +1,16 @@
 import dayjs from "dayjs";
-export function DeliverDate({ selectedDeliveryOption }) {
+export function DeliverDate({ deliverOptions, cartItem }) {
+  const selectedDeliveryOption = deliverOptions.find((deliveryOption) => {
+    return deliveryOption.id === cartItem.deliveryOptionId;
+  });
   return (
-    <div className="delivery-date">
-      Delivery date:{" "}
-      {dayjs(selectedDeliveryOption.estimatedDeliveryTimeMs).format(
-        "dddd, MMMM D",
-      )}
-    </div>
+    <>
+      <div className="delivery-date">
+        Delivery date:{" "}
+        {dayjs(selectedDeliveryOption.estimatedDeliveryTimeMs).format(
+          "dddd, MMMM D",
+        )}
+      </div>
+    </>
   );
 }
--- ecommerce-project/src/Pages/checkout/OrderSummary.jsx
+++ ecommerce-project/src/Pages/checkout/OrderSummary.jsx
@@ -7,15 +7,12 @@ export function OrderSummary({ deliverOptions, cart }) {
     <div className="order-summary">
       {deliverOptions.length > 0 &&
         cart.map((cartItem) => {
-          const selectedDeliveryOption = deliverOptions.find(
-            (deliveryOption) => {
-              return deliveryOption.id === cartItem.deliveryOptionId;
-            },
-          );
-
           return (
             <div key={cartItem.productId} className="cart-item-container">
-              <DeliverDate selectedDeliveryOption={selectedDeliveryOption} />
+              <DeliverDate
+                deliverOptions={deliverOptions}
+                cartItem={cartItem}
+              />
 
               <div className="cart-item-details-grid">
                 <CartItemDetails cartItem={cartItem} />
