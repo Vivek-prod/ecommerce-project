dcd659c750679ce5c88cc3a9c3a291160b692741
Vivek <krishnacreations745@gmail.com>
2026-08-25 14:41:24 +0530

    created OrderDetailsGrid component

--- ecommerce-project/src/Pages/orders/OrdersGrid.jsx
+++ ecommerce-project/src/Pages/orders/OrdersGrid.jsx
@@ -1,8 +1,5 @@
-import { Link } from "react-router";
-import dayjs from "dayjs";
-
-import { Fragment } from "react";
 import { OrderHeader } from "./OrderHEader";
+import { OrderDetailsGrid } from "./OrderDetailsGrid";
 
 export function OrdersGrid({ orders }) {
   return (
@@ -11,48 +8,7 @@ export function OrdersGrid({ orders }) {
         return (
           <div key={order.id} className="order-container">
             <OrderHeader order={order} />
-
-            <div className="order-details-grid">
-              {order.products.map((orderProduct) => {
-                return (
-                  <Fragment key={orderProduct.id}>
-                    <div className="product-image-container">
-                      <img src={orderProduct.product.image} />
-                    </div>
-
-                    <div className="product-details">
-                      <div className="product-name">
-                        {orderProduct.product.name}
-                      </div>
-                      <div className="product-delivery-date">
-                        Arriving on:{" "}
-                        {dayjs(orderProduct.estimatedDeliveryTimeMs).format(
-                          "MMMM D",
-                        )}
-                      </div>
-                      <div className="product-quantity">
-                        Quantity: {orderProduct.quantity}
-                      </div>
-                      <button className="buy-again-button button-primary">
-                        <img
-                          className="buy-again-icon"
-                          src="images/icons/buy-again.png"
-                        />
-                        <span className="buy-again-message">Add to Cart</span>
-                      </button>
-                    </div>
-
-                    <div className="product-actions">
-                      <Link to="/tracking">
-                        <button className="track-package-button button-secondary">
-                          Track package
-                        </button>
-                      </Link>
-                    </div>
-                  </Fragment>
-                );
-              })}
-            </div>
+            <OrderDetailsGrid order={order} />
           </div>
         );
       })}
