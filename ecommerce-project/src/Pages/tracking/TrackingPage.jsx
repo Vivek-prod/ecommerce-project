bf6831b20897473a47da6f887d85df66fca21d32
Vivek <krishnacreations745@gmail.com>
2026-08-25 14:09:12 +0530

    created OrderTracking component for tracking page

--- ecommerce-project/src/Pages/tracking/TrackingPage.jsx
+++ ecommerce-project/src/Pages/tracking/TrackingPage.jsx
@@ -1,6 +1,6 @@
-import { Link } from "react-router";
 import { Header } from "../../Components/Header";
 import "./TrackingPage.css";
+import { OrderTracking } from "./OrderTracking";
 
 export function TrackingPage({ cart }) {
   return (
@@ -10,34 +10,7 @@ export function TrackingPage({ cart }) {
       <Header cart={cart} />
 
       <div className="tracking-page">
-        <div className="order-tracking">
-          <Link className="back-to-orders-link link-primary" href="/orders">
-            View all orders
-          </Link>
-
-          <div className="delivery-date">Arriving on Monday, June 13</div>
-
-          <div className="product-info">
-            Black and Gray Athletic Cotton Socks - 6 Pairs
-          </div>
-
-          <div className="product-info">Quantity: 1</div>
-
-          <img
-            className="product-image"
-            src="images/products/athletic-cotton-socks-6-pairs.jpg"
-          />
-
-          <div className="progress-labels-container">
-            <div className="progress-label">Preparing</div>
-            <div className="progress-label current-status">Shipped</div>
-            <div className="progress-label">Delivered</div>
-          </div>
-
-          <div className="progress-bar-container">
-            <div className="progress-bar"></div>
-          </div>
-        </div>
+        <OrderTracking />
       </div>
     </>
   );
