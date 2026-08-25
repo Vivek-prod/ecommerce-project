97c8170426e73ba0cd94f5b2abf37028adca9ccd
Vivek <krishnacreations745@gmail.com>
2026-08-25 14:38:42 +0530

    created OrderHeader Component

--- ecommerce-project/src/Pages/orders/OrdersGrid.jsx
+++ ecommerce-project/src/Pages/orders/OrdersGrid.jsx
@@ -1,7 +1,8 @@
 import { Link } from "react-router";
 import dayjs from "dayjs";
-import { formatMoney } from "../../utils/money";
+
 import { Fragment } from "react";
+import { OrderHeader } from "./OrderHEader";
 
 export function OrdersGrid({ orders }) {
   return (
@@ -9,23 +10,7 @@ export function OrdersGrid({ orders }) {
       {orders.map((order) => {
         return (
           <div key={order.id} className="order-container">
-            <div className="order-header">
-              <div className="order-header-left-section">
-                <div className="order-date">
-                  <div className="order-header-label">Order Placed:</div>
-                  <div>{dayjs(order.orderTimeMs).format("MMMM D")}</div>
-                </div>
-                <div className="order-total">
-                  <div className="order-header-label">Total:</div>
-                  <div>{formatMoney(order.totalCostCents)}</div>
-                </div>
-              </div>
-
-              <div className="order-header-right-section">
-                <div className="order-header-label">Order ID:</div>
-                <div>{order.id}</div>
-              </div>
-            </div>
+            <OrderHeader order={order} />
 
             <div className="order-details-grid">
               {order.products.map((orderProduct) => {
